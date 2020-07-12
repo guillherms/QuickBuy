c95e40d8fa008023145c61b92dca3ebb8d7ea71e
Guilherme Santos <guisantosoliv@gmail.com>
2020-07-12 00:37:10 -0300

    Criando Repositórios

---
+++ QuickBuy.Repositorio/Repositorios/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class Clientes
+    {
+        public Clientes()
+        {
+            var usuarioRepositorio = new UsuarioRepositorio();
+            usuarioRepositorio.Adicionar(usuario);
+        }
+    }
+}
