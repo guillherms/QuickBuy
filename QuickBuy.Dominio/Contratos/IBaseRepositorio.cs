1b62abfa786b515baaa985dab634c23ca3b449d5
Guilherme Santos <guisantosoliv@gmail.com>
2020-07-11 22:02:41 -0300

    Crinado interfaces

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IBaseRepositorio<TEntity>: IDisposable where TEntity: class
+    {
+        void Adicionar(TEntity entity);
+        TEntity ObterId(int id);
+        IEnumerable<TEntity> ObterTodos();
+        void Remover(TEntity entity);
+        void Atualizar(TEntity entity);
+
+    }
+}
